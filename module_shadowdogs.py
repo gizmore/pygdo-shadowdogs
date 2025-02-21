fcdb1c218be06337d46cbe8cc621b0a8eaf5f8c3
gizmore <gizmore@wechall.net>
2025-02-21 05:00:31 +0100

    'sync'

--- module_shadowdogs.py
+++ module_shadowdogs.py
@@ -1,10 +1,8 @@
 from gdo.base.GDO_Module import GDO_Module
-from gdo.shadowdogs.Player import GDO_Player
 
 
 class module_shadowdogs(GDO_Module):
 
     def gdo_classes(self):
         return [
-            GDO_Player,
         ]
