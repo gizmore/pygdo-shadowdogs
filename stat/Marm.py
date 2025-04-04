c5fdea3adb54dd16c7b9d94e0f62a49b9d8e8515
gizmore <gizmore@wechall.net>
2025-04-04 23:14:33 +0200

    'sd'

---
+++ stat/Marm.py
@@ -0,0 +1,11 @@
+from gdo.shadowdogs.engine.Modifier import Modifier
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from gdo.shadowdogs.GDO_Player import GDO_Player
+
+
+class Marm(Modifier):
+
+    def apply(self, target: 'GDO_Player'):
+        pass
