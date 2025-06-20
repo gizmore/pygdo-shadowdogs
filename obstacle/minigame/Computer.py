696bdb90380c59dd0b053e21bb016f5493b47241
gizmore <gizmore@wechall.net>
2025-06-20 12:28:32 +0200

    'sd'

--- obstacle/minigame/Computer.py
+++ obstacle/minigame/Computer.py
@@ -1,16 +1,28 @@
 from gdo.base.Util import Random
 from gdo.shadowdogs.obstacle.Obstacle import Obstacle
+from gdo.ui.WithSize import WithSize
 
 
-class Computer(Obstacle):
+class Computer(WithSize, Obstacle):
 
     _width: int
     _height: int
 
-    def width(self, w: int):
+    def width(self, width: int):
+        self._width = width
         return self
 
-    def on_hack(self):
-        p = self.get_player()
+    def height(self, height: int):
+        self._height = height
+        return self
 
-        with Random(p.gb('p_seed')):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self._width = 3
+        self._height = 3
+
+
+    def on_hack(self):
+        pass
+
+
