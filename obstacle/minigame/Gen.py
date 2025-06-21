bfc06a531c3fc6892d3db69be7062ab55db07ebb
gizmore <gizmore@wechall.net>
2025-06-21 05:21:35 +0200

    'sd'

--- obstacle/minigame/Gen.py
+++ obstacle/minigame/Gen.py
@@ -1,3 +1,4 @@
+from gdo.base.Util import Random
 from gdo.shadowdogs.WithShadowFunc import WithShadowFunc
 from gdo.shadowdogs.obstacle.minigame.Computer import Computer
 from gdo.shadowdogs.obstacle.minigame.Map import Map
@@ -6,6 +7,6 @@ from gdo.shadowdogs.obstacle.minigame.Map import Map
 class Gen(WithShadowFunc):
     def generate(self, computer: Computer) -> Map:
         p = self.get_player()
-        scramble = computer.gobs('scrambled', 0)
+        scramble = computer.gobs('scrambled') or 0
         with Random(int(p.gb('p_seed')) + int(scramble)):
-        pass
+            pass
