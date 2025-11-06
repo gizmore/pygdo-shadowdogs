4b9bfbc2327baff610918d03c0b427cd76d96a12
Christian Busch <cb@oncrea.de>
2025-11-06 21:55:42 +0100

    'sd'

--- npcs/npcs.py
+++ npcs/npcs.py
@@ -7,16 +7,16 @@ class npcs:
 
     NPCS = {
         'reaper':   {'klass': Mob,    'p_level': 999, 'p_max_hp': (100, 100)},
-        'police':   {'klass': Police, 'eq': ['SteelToes', 'KevlarVest', '9mm Pistol', 'KevlarLeggings']},
+        'police':   {'klass': Police, 'eq': ['SteelToes', 'KevlarVest', 'Revolver', 'KevlarLeggings']},
 
         'lamer':    {'klass': Mob,     'p_level': 1,                  'p_max_hp': (1, 2), 'eq': ['Trousers', 'TShirt']},
         'haider':   {'klass': Mob,     'p_level': 2,                  'p_max_hp': (1, 3), 'eq': ['Sandals', 'Trousers', 'TShirt']},
         'noob':     {'klass': Mob,     'p_level': 3,                  'p_max_hp': (2, 3), 'eq': ['RagShoes', 'Trousers', 'Jacket']},
         'robber':   {'klass': Mob,     'p_level': 4,                  'p_max_hp': (2, 4), 'eq': ['RagShoes', 'Trousers', 'Jacket']},
-        'gangster': {'klass': Mob,     'p_level': 5,                  'p_max_hp': (3, 4), 'eq': ['Shoes', 'Trousers', 'Pullover', 'Bytegun']},
-        'goblin':   {'klass': Mob,     'p_level': 6, 'p_race': 'ork', 'p_max_hp': (3, 5), 'eq': ['Shoes', 'Trousers', 'Pullover', 'Bytegun']},
+        'gangster': {'klass': Mob,     'p_level': 5,                  'p_max_hp': (3, 4), 'eq': ['Shoes', 'Trousers', 'Pullover', 'PipePistol']},
+        'goblin':   {'klass': Mob,     'p_level': 6, 'p_race': 'ork', 'p_max_hp': (3, 5), 'eq': ['Shoes', 'Trousers', 'Pullover', 'PipePistol']},
         'ork':      {'klass': HardMob, 'p_level': 8, 'p_race': 'ork', 'p_max_hp': (4, 6), 'eq': ['CombatBoots', 'Jeans', 'LeatherVest', 'Sword']},
-        'peine_cop': {'klass': HardMob, 'p_level': 8, 'p_race': 'human', 'p_max_hp': (6, 8), 'eq': ['CombatBoots', 'CombatJeans', 'CombatVest', 'Baretta']},
+        'peine_cop': {'klass': HardMob, 'p_level': 8, 'p_race': 'human', 'p_max_hp': (6, 8), 'eq': ['CombatBoots', 'CombatJeans', 'CombatVest', 'Pistol']},
 
         'wolf':     {'klass': Mob,     'p_level': 3,                  'p_max_hp': (4, 6), 'p_max_dmg': 1, 'eq': {'Claws'}},
     }
