58fd3d982786f56622d408276ca314aa30044fbc
gizmore <gizmore@wechall.net>
2025-11-06 23:51:24 +0100

    'sd'

--- city/y2064/Peine/locations/bank/Bank.py
+++ city/y2064/Peine/locations/bank/Bank.py
@@ -1,4 +1,10 @@
+from gdo.shadowdogs.city.y2064.Peine.locations.bank.Clerk import Clerk
 from gdo.shadowdogs.locations.Bank import Bank as BaseBank
+from gdo.shadowdogs.npcs.TalkingNPC import TalkingNPC
+
 
 class Bank(BaseBank):
-    pass
+
+    NPCS: 'list[type[TalkingNPC]]' = [
+        Clerk,
+    ]
