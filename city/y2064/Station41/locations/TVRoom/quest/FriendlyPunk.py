fa0df57ab5c64aacee5dbc914740b68f1e968701
gizmore <gizmore@wechall.net>
2026-08-12 15:02:37 +0200

    Add Friendly Punk and Cleansy quest stubs

---
+++ city/y2064/Station41/locations/TVRoom/quest/FriendlyPunk.py
@@ -0,0 +1,8 @@
+from gdo.shadowdogs.SD_Quest import SD_Quest
+
+
+class FriendlyPunk(SD_Quest):
+    """"
+    How a new inmate brought hash into the clinics and we smoked it ... panic attack,,, crazy thoughts... maybe fight a ghost or something
+    """
+    pass
---
+++ city/y2088/Entgiftung/Cleansy.py
@@ -0,0 +1,9 @@
+from gdo.shadowdogs.SD_Quest import SD_Quest
+
+
+class Cleansy(SD_Quest):
+    """
+    Borrow the master key from the cleaning lady and ask her how much she earns.
+    Steal a table tennis racket and medics from the room behind reception.
+    (Made up, but dedicated to a special cleansly lady with humor.
+    """
