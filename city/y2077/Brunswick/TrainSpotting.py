9252f05a22ba9ad21a3ae4ad77578192935aa143
gizmore <gizmore@wechall.net>
2026-08-13 07:07:57 +0200

    Trainspotting draft

---
+++ city/y2077/Brunswick/TrainSpotting.py
@@ -0,0 +1,9 @@
+from gdo.shadowdogs.SD_Quest import SD_Quest
+
+
+class TrainSpotting(SD_Quest):
+    """
+    Im Zug durchdrehen und alle anpöbeln das wir doch was tun müssen um die welt zu retten.
+    VOrher den Bahnhof aufräumen.
+    """
+    pass
