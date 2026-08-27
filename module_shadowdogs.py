39393ab9390d544cc127d52362e7549d1587b5a4
gizmore <gizmore@wechall.net>
2026-08-27 09:46:13 +0200

    lang and fix

--- module_shadowdogs.py
+++ module_shadowdogs.py
@@ -70,11 +70,11 @@ class module_shadowdogs(GDO_Module):
             Loader.load_npcs()
             Loader.load_parties()
 
-    def gdo_init_sidebar(self, page: 'GDT_Page'):
-        page._right_bar.add_field(GDT_Link().href(href('shadowdogs', 'shadowdogs')).text('module_shadowdogs'))
+    # def gdo_init_sidebar(self, page: 'GDT_Page'):
+    #     page._right_bar.add_field(GDT_Link().href(href('shadowdogs', 'shadowdogs')).text('module_shadowdogs'))
 
-    def gdo_load_scripts(self, page: 'GDT_Page'):
-        self.add_js('js/sd.js')
+    # def gdo_load_scripts(self, page: 'GDT_Page'):
+    #     self.add_js('js/sd.js')
 
     ##########w
     # Config #
